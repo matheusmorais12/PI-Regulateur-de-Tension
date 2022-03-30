0e8c14d8b78f41660c374ad7dbde490f6cd81f28
MORAIS Matheus <matheus.morais@ee.ufcg.edu.br>
2022-03-30 09:45:41 +0200

    up

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,9 @@ class PID
 public:
   double commande;
   double consigne;
-  double mesure;
+  double mesure[2] = {};
   double alpha, beta, gama;
   double alpha1, beta1, gama1;
-  double 
 
   PID(double _alpha, double _beta, double _gama, double _alpha1, double _beta1, double _gama1)
   {
@@ -19,23 +18,28 @@ public:
     beta1 = _beta1;
     gama1 = _gama1;
   }
-  //Precisamos de tres variaveis para cada um, antes, o atual e o calculado. 
-  void addNewMesure(double _mesure){
-    mesure = _mesure; //leitura do sensor 
+  // Precisamos de tres variaveis para cada um, antes, o atual e o calculado.
+  double addNewMesure(double _mesure)
+  {
+    mesure[0] = mesure[1];
+    mesure[1] = _mesure;
+
+    return mesure
   }
 
-  void addNewConsigne(double _consigne){
-    consigne = _consigne; //saida do controlador 
+  void addNewConsigne(double _consigne)
+  {
+    consigne = _consigne; // saida do controlador
   }
 
-  void addNewCommande(double _commande){
-    commande = _commande; //sinal entre PI e sistema
+  void addNewCommande(double _commande)
+  {
+    commande = _commande; // sinal entre PI e sistema
   }
 
-
-  double Function_controled(){
-    commande = 1/alpha*(alpha*consigne-beta*consigne+gama*consigne-alpha*mesure-beta*mesure+gama*mesure+beta1*commande-gama1*mesure);
-
+  double Function_controled(double _mesure)
+  {
+    commande = 1 / alpha1 * (alpha * consigne - beta * consigne + gama * consigne - alpha * _mesure - beta * mesure[0] + gama * mesure[1] + beta1 * commande - gama1 * commande);
   }
 };
 
@@ -54,6 +58,6 @@ void setup()
 void loop()
 {
   double mesure = map(analogRead(Sensor), 0, 1023, 0, 3);
+  pid_control.addNewCommande(mesure);
   pid_control.addNewMesure(mesure);
-
 }
